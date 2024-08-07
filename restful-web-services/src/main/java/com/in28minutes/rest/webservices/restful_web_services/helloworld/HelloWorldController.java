0ed4f2d71daa00e0daf2b7d34b9aecbd4159c4aa
Aravind Anand <itsaravindanand@gmail.com>
2024-08-06 20:55:28 -0700

    Content Negotiation and Internationalization

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
@@ -1,13 +1,23 @@
 package com.in28minutes.rest.webservices.restful_web_services.helloworld;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 //Expose REST API
 @RestController
 public class HelloWorldController {
-    //URL: /helloWorld
+
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+//URL: /helloWorld
     //Return Hello World
 
     //Direct String returned
@@ -31,5 +41,13 @@ public class HelloWorldController {
         //return new HelloWorldBean("Hello World, "+ name);
         //Using String formatter
         return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
+
+    //Hello World I18N
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized() {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message",null,"Default Message",locale);
+        //return "Hello World V2";
     }
 }

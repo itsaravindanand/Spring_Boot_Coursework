0c72ffc44dd1a12ba963ed88a1213a17f16e8653
Aravind Anand <itsaravindanand@gmail.com>
2024-08-09 21:10:57 -0700

    Migration to MySQL and Authentication for using REST API

---
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/security/SpringSecurityConfiguration.java
@@ -0,0 +1,32 @@
+package com.in28minutes.rest.webservices.restful_web_services.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SpringSecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+//		1) All requests should be authenticated
+        http.authorizeHttpRequests(
+                auth -> auth.anyRequest().authenticated()
+        );
+//		2) If a request is not authenticated, use http basic
+        http.httpBasic(withDefaults());
+
+//		3) CSRF -> POST, PUT
+        http.csrf(csrf -> csrf.disable());
+//      OR
+//		http.csrf(AbstractHttpConfigurer::disable);
+
+
+        return http.build();
+    }
+
+}

14bb3521b431244f56057b37ea7c80251cf17b17
Aravind Anand <itsaravindanand@gmail.com>
2024-08-18 16:35:02 -0700

    Exploring Gradle
    Practiced tasks and groovy scripts

---
+++ learn-gradle/src/test/java/com/in28minutes/learn_gradle/LearnGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.learn_gradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

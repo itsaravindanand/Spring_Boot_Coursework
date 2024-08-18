ab9f48092182101a51289a7a288e2e3d7d6251d3
Aravind Anand <itsaravindanand@gmail.com>
2024-08-18 16:06:50 -0700

    Exploring Maven
    Practiced some important Maven commands with the Maven Project

---
+++ learn-maven/src/main/java/com/in28minutes/learn_maven/LearnMavenApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.learn_maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnMavenApplication.class, args);
+	}
+
+}
---
+++ learn-maven/src/test/java/com/in28minutes/learn_maven/LearnMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.learn_maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
